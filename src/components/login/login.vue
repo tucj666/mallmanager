12429fec6926f48f60549ee624579cea190c92a5
capser <13530618523@163.com>
2021-12-09 17:38:48 +0800

    新建vue组件和路由配置

---
+++ src/components/login/login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        登录
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VueprojectLogin',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
